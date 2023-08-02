deb8b5c7654fbab389d0cb11a7fd428c4ee71238
Mustafa Ülker <mustafa.ulker@ndk.org.tr>
2023-08-02 15:22:42 +0300

    singleton pattern

---
+++ DesignPattern.SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesignPattern.SingletonPattern;
+
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+
+foreach (var country in countries)
+{
+    Console.WriteLine(country.Name);
+}
+
+// Another Service ...
+
+//var countryProvider1 = new CountryProvider(); //burda bize kızıyor çünki private yaptık ctor unu bir kez daha create edemezsin diyor bize 
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries1 = await CountryProvider.Instance.GetCountries();
---
+++ DesignPattern.SingletonPattern/countryProvider.cs
@@ -0,0 +1,33 @@
+﻿using DesignPattern.SingletonPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.SingletonPattern
+{
+    public class CountryProvider
+    {
+
+        private static CountryProvider instance = null;
+        public static CountryProvider Instance => instance ?? (instance = new CountryProvider());
+
+        private new List<Country> Countries { get; set; }
+
+        private CountryProvider()
+        {
+                // Retrieving data from db
+                Task.Delay(1000).GetAwaiter().GetResult();    
+
+                Countries = new List<Country>()
+                {
+                    new Country(){Name = "Turkey"},
+                    new Country(){Name = "United Kingdom"}
+                };
+        }
+
+        public async Task<List<Country>> GetCountries() => Countries;
+    }
+}
+ 
